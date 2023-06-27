157d7887f087611d699790715efbf36be9ccedcf
Sami Albayrak <98155908+CoolerM4nn@users.noreply.github.com>
2023-06-27 21:29:38 +0200

    Add posibility to play a round of Rock, Paper, Sciccors

--- game.js
+++ game.js
@@ -2,19 +2,75 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+function roundOfRPS() {
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  switch (playerChoice) {
+    case 'ROCK':
+      switch (computerChoice) {
+        case 'ROCK':
+          return 'Draw';
+        case 'PAPER':
+          return 'Paper beats Rock, you lose.';
+        case 'SCICCORS':
+          return 'Rock beats Sciccors, you win.';
+
+        default:
+          break;
+      }
+      break;
+    case 'PAPER':
+      switch (computerChoice) {
+        case 'ROCK':
+          return 'Paper beats Rock, you win.';
+        case 'PAPER':
+          return 'Draw';
+        case 'SCICCORS':
+          return 'Sciccors beat Paper, you lose.';
+
+        default:
+          break;
+      }
+      break;
+
+    case 'SCICCORS':
+      switch (computerChoice) {
+        case 'ROCK':
+          return 'Rock beats Sciccors, you lose.';
+        case 'PAPER':
+          return 'Sciccors beat Rock, you win.';
+        case 'SCICCORS':
+          return 'Draw';
+
+        default:
+          break;
+      }
+      break;
+
+    default:
+      console.log('Only Rock, Paper or Scissors are allowed!');
+      break;
+  }
+}
+
+function getPlayerChoice() {
+  return prompt().toUpperCase();
+}
+
 function getComputerChoice() {
   switch (getRandomInt(3)) {
     case 0:
-      return 'Rock';
+      return 'ROCK';
 
     case 1:
-      return 'Paper';
+      return 'PAPER';
 
     case 2:
-      return 'Sciccors';
+      return 'SCICCORS';
 
     default:
       break;
   }
 }
 
+console.log(roundOfRPS());
